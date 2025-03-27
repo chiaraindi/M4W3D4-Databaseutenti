3e5fda1cff871e7ba16c66fa8f5623aeeb297afa
Chiara Indirli <chiaraindirli2@gmail.com>
2025-03-27 20:37:18 +0100

    correct

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const tableDataContainer: HTMLElement | null = document.getElementById("table-data");
+const tableDataContainer = document.getElementById('table-data');
 const API = "https://jsonplaceholder.typicode.com/users";
 
